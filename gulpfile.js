5cc8eb75fdd28400d9f6009cebf28fdbf3b3ebba
Hugo <hhugo.avila97@gmail.com>
2023-05-04 15:16:33 -0400

    agregando tareas por default

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const {src, dest, watch} = require('gulp');
+const {src, dest, watch, series, parallel} = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
@@ -21,3 +21,7 @@ const dev = () => {
 
 exports.css = css;
 exports.dev = dev;
+exports.default = series( css, dev );
+
+// series - Se inicia una tarea, y hasta que finaliza, inicia la siguiente
+// parallel - Todas inician al mismo tiempo

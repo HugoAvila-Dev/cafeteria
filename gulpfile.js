c11429f7994fd20922568c31cefc270a94e385a4
Hugo <hhugo.avila97@gmail.com>
2023-05-06 16:09:03 -0400

    imagenes aligeradas

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,13 @@
 const {src, dest, watch, series, parallel} = require('gulp');
+
+// CSS Y SASS
 const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
+
+// Imagenes
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
 
 const css = ( done ) => {
     //compilar sass 
@@ -14,14 +20,26 @@ const css = ( done ) => {
 
         done();
 }
-
+const imagenes = () => {
+    return src('src/img/**/*')
+    .pipe( imagemin({ optimizationLevel: 3 }) )
+    .pipe( dest('build/img') );
+}
+const versionWebp = () => {
+    return src('src/img/**/*.{png,jpg}')
+    .pipe( webp() )
+    .pipe( dest( 'build/img' ) )
+} 
 const dev = () => {
     watch( 'src/scss/**/*.scss', css);
+    watch( 'src/img/**/*', imagenes );
 }
 
 exports.css = css;
 exports.dev = dev;
-exports.default = series( css, dev );
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.default = series( imagenes, versionWebp, css, dev );
 
 // series - Se inicia una tarea, y hasta que finaliza, inicia la siguiente
 // parallel - Todas inician al mismo tiempo
